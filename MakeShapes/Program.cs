ff0f4db0b0a45e7841aeb29de49f2c91139180f3
Steven Fast <spfastjr@gmail.com>
2014-11-25 12:38:34 -0500

    Uploaded code to github

---
+++ MakeShapes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeShapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> s = new List<Shape>();
+            s.Add(new Triangle { });
+            s.Add(new Square { });
+            s.Add(new Circle { });
+            s.Add(new Shape { });
+            foreach (Shape shape in s)
+            {
+                shape.Print();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MakeShapes/Shape.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeShapes
+{
+    internal class Shape
+    {
+        internal virtual void Print()
+        {
+            Console.WriteLine("I am a generic shape.");
+        }
+    }
+    internal class Circle:Shape
+    {
+        internal override void Print()
+        {
+            Console.WriteLine("I am a circle.");
+        }
+    }
+    internal class Square:Shape
+    {
+        internal override void Print()
+        {
+            Console.WriteLine("I am a square.");
+        }
+    }
+    internal class Triangle:Shape
+    {
+        internal override void Print()
+        {
+            Console.WriteLine("I am a triangle.");
+        }
+    }
+}
